c97b41498bce3c4e93dda88172b177bc7188350e
sapr797 <unisapr797@gmail.com>
2026-06-25 02:45:36 +0300

    Update variables.tf

--- Zad2/variables.tf
+++ Zad2/variables.tf
@@ -1,3 +1,4 @@
+# Общие
 variable "cloud_id" {
   description = "Yandex Cloud ID"
   type        = string
@@ -8,12 +9,19 @@ variable "folder_id" {
   type        = string
 }
 
+variable "yc_token" {
+  description = "Yandex Cloud IAM token"
+  type        = string
+  sensitive   = true
+}
+
 variable "service_account_key_file" {
   description = "Path to service account key file"
   type        = string
   default     = "../01-setup/key.json"
 }
 
+# Сеть
 variable "zones" {
   description = "Availability zones with CIDR blocks"
   type = map(object({
@@ -32,18 +40,32 @@ variable "zones_list" {
   default     = ["ru-central1-a", "ru-central1-b", "ru-central1-d"]
 }
 
+variable "vpc_name" {
+  description = "VPC network name"
+  type        = string
+  default     = "k8s-network"
+}
+
+# Кластер
+variable "cluster_name" {
+  description = "Kubernetes cluster name"
+  type        = string
+  default     = "k8s-cluster"
+}
+
+variable "kubernetes_version" {
+  description = "Kubernetes version"
+  type        = string
+  default     = "1.30"
+}
+
 variable "k8s_api_allowed_cidrs" {
   description = "CIDRs allowed to access Kubernetes API"
   type        = list(string)
   default     = ["0.0.0.0/0"]
 }
 
-variable "ssh_public_key_path" {
-  description = "Path to SSH public key"
-  type        = string
-  default     = "~/.ssh/id_rsa.pub"
-}
-
+# Worker nodes
 variable "worker_count" {
   description = "Number of worker nodes"
   type        = number
@@ -74,8 +96,29 @@ variable "worker_disk_size" {
   default     = 20
 }
 
+variable "preemptible" {
+  description = "Use preemptible VMs for workers"
+  type        = bool
+  default     = true
+}
+
+# SSH
+variable "ssh_public_key_path" {
+  description = "Path to SSH public key"
+  type        = string
+  default     = "~/.ssh/id_rsa.pub"
+}
+
+# Образ
 variable "image_id" {
   description = "Ubuntu 22.04 LTS image ID"
   type        = string
   default     = "fd80ok8sil1fn2gqbm6h"
+}
+
+# Backend
+variable "state_bucket_name" {
+  description = "S3 bucket name for Terraform state"
+  type        = string
+  default     = "tf-state-k8s"
 }
