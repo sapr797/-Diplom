291a9f65123c72c356ed85689ced079d101ff842
sapr797 <unisapr797@gmail.com>
2026-06-25 02:55:19 +0300

    Update main.tf

--- Zad2/main.tf
+++ Zad2/main.tf
@@ -1,5 +1,13 @@
 terraform {
   required_version = ">= 1.3"
+
+  backend "s3" {
+    endpoint   = "storage.yandexcloud.net"
+    region     = "ru-central1"
+    skip_region_validation      = true
+    skip_credentials_validation = true
+  }
+
   required_providers {
     yandex = {
       source  = "yandex-cloud/yandex"
