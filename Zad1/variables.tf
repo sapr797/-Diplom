37677b67ea5c3743c8b076f6bb0a2d9cede2b97f
sapr797 <unisapr797@gmail.com>
2026-06-25 02:21:23 +0300

    Update variables.tf

--- Zad1/variables.tf
+++ Zad1/variables.tf
@@ -8,10 +8,10 @@ variable "folder_id" {
   type        = string
 }
 
-variable "service_account_key_file" {
-  description = "Path to service account key file"
+variable "yc_token" {
+  description = "Yandex Cloud IAM token"
   type        = string
-  default     = "key.json"
+  sensitive   = true
 }
 
 variable "zone" {
@@ -20,21 +20,50 @@ variable "zone" {
   default     = "ru-central1-a"
 }
 
-variable "bucket_name_prefix" {
-  description = "Prefix for S3 bucket name"
+variable "cluster_name" {
+  description = "Name of the Kubernetes cluster"
   type        = string
-  default     = "tf-state"
+  default     = "k8s-cluster"
 }
 
-variable "environment" {
-  description = "Environment name"
+variable "cluster_description" {
+  description = "Description of the Kubernetes cluster"
   type        = string
-  default     = "dev"
+  default     = "Managed Kubernetes cluster"
 }
 
-variable "yc_token" {
-  description = "Yandex Cloud IAM token (alternative to service account key)"
+variable "node_count" {
+  description = "Number of worker nodes"
+  type        = number
+  default     = 2
+}
+
+variable "node_disk_size" {
+  description = "Size of node disk in GB"
+  type        = number
+  default     = 30
+}
+
+variable "node_memory" {
+  description = "Memory for each node in GB"
+  type        = number
+  default     = 4
+}
+
+variable "node_cores" {
+  description = "CPU cores for each node"
+  type        = number
+  default     = 2
+}
+
+variable "node_core_fraction" {
+  description = "Core fraction for nodes (20, 50, 100)"
+  type        = number
+  default     = 50
+}
+
+variable "subnet_cidr" {
+  description = "CIDR block for subnet"
   type        = string
-  sensitive   = true
-  default     = null
+  default     = "10.100.0.0/24"
 }
