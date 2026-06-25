4bfc798873c8e9795bfead9e6ca89dd39bfd5b30
sapr797 <unisapr797@gmail.com>
2026-06-25 03:47:01 +0300

    Update outputs.tf

--- Zad1/outputs.tf
+++ Zad1/outputs.tf
@@ -1,40 +1,64 @@
-output "cluster_id" {
-  description = "ID of the Kubernetes cluster"
-  value       = yandex_kubernetes_cluster.k8s_cluster.id
+# --- Информация о сервисном аккаунте ---
+output "service_account_id" {
+  description = "ID созданного сервисного аккаунта"
+  value       = yandex_iam_service_account.terraform_sa.id
 }
 
-output "cluster_name" {
-  description = "Name of the Kubernetes cluster"
-  value       = yandex_kubernetes_cluster.k8s_cluster.name
+output "service_account_name" {
+  description = "Имя созданного сервисного аккаунта"
+  value       = yandex_iam_service_account.terraform_sa.name
 }
 
-output "cluster_endpoint" {
-  description = "API endpoint of the Kubernetes cluster"
-  value       = yandex_kubernetes_cluster.k8s_cluster.master.0.external_v4_endpoint
+output "service_account_email" {
+  description = "Email созданного сервисного аккаунта"
+  value       = yandex_iam_service_account.terraform_sa.email
 }
 
-output "cluster_ca_certificate" {
-  description = "CA certificate for the Kubernetes cluster"
-  value       = yandex_kubernetes_cluster.k8s_cluster.master.0.cluster_ca_certificate
+# --- Ключи сервисного аккаунта ---
+output "service_account_access_key" {
+  description = "Access key для сервисного аккаунта"
+  value       = yandex_iam_service_account_static_access_key.sa_key.access_key
   sensitive   = true
 }
 
-output "node_group_id" {
-  description = "ID of the node group"
-  value       = yandex_kubernetes_node_group.k8s_nodes.id
+output "service_account_secret_key" {
+  description = "Secret key для сервисного аккаунта"
+  value       = yandex_iam_service_account_static_access_key.sa_key.secret_key
+  sensitive   = true
 }
 
-output "node_group_name" {
-  description = "Name of the node group"
-  value       = yandex_kubernetes_node_group.k8s_nodes.name
+# --- Информация о бакете ---
+output "bucket_name" {
+  description = "Имя созданного S3 бакета"
+  value       = yandex_storage_bucket.tf_state.bucket
 }
 
-output "subnet_id" {
-  description = "ID of the subnet"
-  value       = yandex_vpc_subnet.k8s_subnet.id
+output "bucket_url" {
+  description = "URL созданного S3 бакета"
+  value       = "s3://${yandex_storage_bucket.tf_state.bucket}"
 }
 
-output "network_id" {
-  description = "ID of the VPC network"
-  value       = yandex_vpc_network.k8s_network.id
+# --- Статус бакета ---
+output "bucket_versioning" {
+  description = "Статус версионирования бакета"
+  value       = yandex_storage_bucket.tf_state.versioning[0].enabled
+}
+
+# --- Теги бакета ---
+output "bucket_tags" {
+  description = "Теги созданного бакета"
+  value       = yandex_storage_bucket.tf_state.tags
+}
+
+# --- Команды для настройки backend ---
+output "backend_commands" {
+  description = "Команды для настройки backend"
+  value       = <<-EOT
+    # Для настройки бекенда в 02-cluster:
+    # Скопируйте эти значения в backend.hcl
+    
+    bucket     = "${yandex_storage_bucket.tf_state.bucket}"
+    access_key = "${yandex_iam_service_account_static_access_key.sa_key.access_key}"
+    secret_key = "${yandex_iam_service_account_static_access_key.sa_key.secret_key}"
+  EOT
 }
